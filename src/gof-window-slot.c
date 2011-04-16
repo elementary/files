5d3dbda43a6163696d35fe0963d5568283d6cac2
xapantu <xapantu@gmail.com>
2011-04-16 13:23:50 -0700

    Build system for the tests and a stub of keyboard handling for the miller columns

--- src/gof-window-slot.c
+++ src/gof-window-slot.c
@@ -38,101 +38,20 @@ enum {
 
 static guint signals[LAST_SIGNAL] = { 0 };
 
-#if 0
-static void
-real_active (NautilusWindowSlot *slot)
-{
-    NautilusWindow *window;
-
-    window = slot->pane->window;
-
-    /* sync window to new slot */
-    nautilus_window_sync_status (window);
-    nautilus_window_sync_allow_stop (window, slot);
-    nautilus_window_sync_title (window, slot);
-    nautilus_window_sync_zoom_widgets (window);
-    nautilus_window_pane_sync_location_widgets (slot->pane);
-    nautilus_window_pane_sync_search_widgets (slot->pane);
-
-    if (slot->viewed_file != NULL) {
-        nautilus_window_load_view_as_menus (window);
-        nautilus_window_load_extension_menus (window);
-    }
-}
-
-static void
-nautilus_window_slot_active (NautilusWindowSlot *slot)
-{
-    NautilusWindow *window;
-    NautilusWindowPane *pane;
-
-    g_assert (NAUTILUS_IS_WINDOW_SLOT (slot));
-
-    pane = NAUTILUS_WINDOW_PANE (slot->pane);
-    window = NAUTILUS_WINDOW (slot->pane->window);
-    g_assert (g_list_find (pane->slots, slot) != NULL);
-    g_assert (slot == window->details->active_pane->active_slot);
-
-    EEL_CALL_METHOD (NAUTILUS_WINDOW_SLOT_CLASS, slot,
-                     active, (slot));
-}
-
-static void
-real_inactive (NautilusWindowSlot *slot)
-{
-    NautilusWindow *window;
-
-    window = NAUTILUS_WINDOW (slot->pane->window);
-    g_assert (slot == window->details->active_pane->active_slot);
-}
-
-static void
-nautilus_window_slot_inactive (NautilusWindowSlot *slot)
-{
-    NautilusWindow *window;
-    NautilusWindowPane *pane;
-
-    g_assert (NAUTILUS_IS_WINDOW_SLOT (slot));
-
-    pane = NAUTILUS_WINDOW_PANE (slot->pane);
-    window = NAUTILUS_WINDOW (pane->window);
-
-    g_assert (g_list_find (pane->slots, slot) != NULL);
-    g_assert (slot == window->details->active_pane->active_slot);
-
-    EEL_CALL_METHOD (NAUTILUS_WINDOW_SLOT_CLASS, slot,
-                     inactive, (slot));
-}
-#endif
-
-
 void
 gof_window_column_add (GOFWindowSlot *slot, GtkWidget *column)
 {
-    //GtkWidget *scrolled;
-
     GtkWidget *hpane = gtk_hpaned_new();
     gtk_widget_show (hpane);
-    //gtk_paned_set_position(GTK_PANED (hpane), 200);
     gtk_container_add(GTK_CONTAINER(slot->colpane), hpane);
-    //gtk_box_pack_end(GTK_BOX(slot->colpane), hpane, TRUE, FALSE, 0);
     GtkWidget *vbox2 = gtk_hbox_new(FALSE, 0);
     gtk_widget_show (vbox2);
     slot->colpane = vbox2;
     slot->hpane = hpane;
 
-    /*scrolled = gtk_scrolled_window_new(0, 0);
-      gtk_widget_show(scrolled);
-      gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW (scrolled), column);
-      gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled),
-      GTK_POLICY_NEVER,
-      GTK_POLICY_AUTOMATIC);*/
-    //gtk_container_add(GTK_CONTAINER(scrolled), column);
     gtk_widget_set_size_request (column, 180,-1);
 
     gtk_paned_pack1 (GTK_PANED (hpane), column, FALSE, FALSE);
-    //gtk_paned_pack1 (GTK_PANED (hpane), column, TRUE, TRUE);
-    //gtk_paned_pack1 (GTK_PANED (hpane), scrolled, FALSE, FALSE);
     gtk_paned_pack2 (GTK_PANED (hpane), vbox2, TRUE, FALSE);
 }
 
@@ -141,8 +60,10 @@ gof_window_columns_add_location (GOFWindowSlot *slot, GFile *location)
 {
     log_printf (LOG_LEVEL_UNDEFINED, "%s\n", G_STRFUNC);
     slot->mwcols->active_slot = slot;
-    //GList *childs = gtk_container_get_children (slot->colpane);
     gtk_container_foreach (GTK_CONTAINER (slot->colpane), (GtkCallback)gtk_widget_destroy, NULL);
+    
+    slot->mwcols->slot = NULL;
+
     marlin_window_columns_add (slot->mwcols, location);
     /*GOFWindowSlot *slot = gof_window_slot_column_new (location);
       slot->mwcols = mwcols;
@@ -181,43 +102,7 @@ gof_window_slot_new (GFile *location, GObject *ctab)
     slot->location = g_object_ref (location);
     slot->ctab = ctab;
 
-    //amtest
-    //slot->directory = gof_directory_async_new(slot->location);
     slot->directory = gof_directory_get (slot->location);
-    /*slot->view_box = GTK_WIDGET (g_object_new (FM_TYPE_LIST_VIEW,
-                                               "window-slot", slot, NULL));*/
-    //slot->view_box = gtk_label_new ("test");
-    //marlin_window_set_active_slot (MARLIN_WINDOW (window), slot);
-    //marlin_view_window_set_active_slot (MARLIN_VIEW_WINDOW (window), slot);
-#if 0
-    slot->colpane = gtk_hbox_new (FALSE, 0);
-    gtk_widget_show (slot->colpane);
-    slot->view_box = gtk_scrolled_window_new(0, 0);
-
-    //gtk_container_add (GTK_CONTAINER (view), hpane);
-    //gtk_container_add (GTK_CONTAINER (slot->view_box), slot->colpane);
-    gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW (slot->view_box), slot->colpane);
-    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (slot->view_box),
-                                    GTK_POLICY_AUTOMATIC,
-                                    GTK_POLICY_NEVER);
-    /*add_column(slot, GTK_WIDGET (g_object_new (FM_TYPE_COLUMNS_VIEW,
-      "window-slot", slot, NULL)));*/
-    /*slot->view_box = GTK_WIDGET (g_object_new (FM_TYPE_COLUMNS_VIEW,
-      "window-slot", slot, NULL));*/
-    gtk_widget_show (slot->view_box);
-
-    /*int i;
-      for (i=0; i<4; i++) {
-      GtkWidget *test = gtk_label_new ("test");
-    //gtk_widget_set_size_request(test, 100,-1);
-    gtk_widget_show(test);
-    add_column(slot, test);
-    }*/
-#endif
-    /*gtk_container_add( GTK_CONTAINER(window), slot->view_box);*/
-    //marlin_view_window_set_content (window, slot->view_box);
-    /*marlin_view_view_container_set_content (ctab, slot->view_box);
-    load_dir_async (slot->directory);*/
 
     return slot;
 }
@@ -242,64 +127,11 @@ gof_window_slot_make_column_view (GOFWindowSlot *slot)
 static void
 gof_window_slot_init (GOFWindowSlot *slot)
 {
-#if 0
-    GtkWidget *content_box, *eventbox, *extras_vbox, *frame, *hsep;
-
-    content_box = gtk_vbox_new (FALSE, 0);
-    slot->content_box = content_box;
-    gtk_widget_show (content_box);
-
-    frame = gtk_frame_new (NULL);
-    gtk_frame_set_shadow_type (GTK_FRAME (frame), GTK_SHADOW_ETCHED_IN);
-    gtk_box_pack_start (GTK_BOX (content_box), frame, TRUE, TRUE, 0);
-    gtk_widget_show (frame);
-
-    slot->view_box = gtk_vbox_new (FALSE, 0);
-    gtk_container_add (GTK_CONTAINER (frame), slot->view_box);
-    gtk_widget_show (slot->view_box);
-#endif
-    //slot->view_box = GTK_WIDGET (g_object_new (FM_TYPE_LIST_VIEW, NULL));
-    /*slot->view_box = GTK_WIDGET (g_object_new (FM_TYPE_LIST_VIEW,
-      "window-slot", slot, NULL));*/
-    //gtk_box_pack_start (GTK_BOX (slot->content_box), GTK_WIDGET (slot->list_view->tree), TRUE, TRUE, 0);
-
-
-    /*GtkWidget *m_scwin;
-      m_scwin = gtk_scrolled_window_new(NULL,NULL);
-      slot->content_box = m_scwin;
-      gtk_scrolled_window_set_policy( GTK_SCROLLED_WINDOW(m_scwin), GTK_POLICY_AUTOMATIC , GTK_POLICY_AUTOMATIC);
-      gtk_scrolled_window_set_shadow_type( GTK_SCROLLED_WINDOW(m_scwin), GTK_SHADOW_NONE);*/
-
-    //gtk_container_add( GTK_CONTAINER(m_scwin), GTK_WIDGET (slot->list_view->tree));
-
-
-#if 0
-    eventbox = gtk_event_box_new ();
-    slot->extra_location_event_box = eventbox;
-    gtk_widget_set_name (eventbox, "nautilus-extra-view-widget");
-    gtk_box_pack_start (GTK_BOX (slot->view_box), eventbox, FALSE, FALSE, 0);
-
-    extras_vbox = gtk_vbox_new (FALSE, 6);
-    gtk_container_set_border_width (GTK_CONTAINER (extras_vbox), 6);
-    slot->extra_location_widgets = extras_vbox;
-    gtk_container_add (GTK_CONTAINER (eventbox), extras_vbox);
-    gtk_widget_show (extras_vbox);
-
-    hsep = gtk_hseparator_new ();
-    gtk_box_pack_start (GTK_BOX (slot->view_box), hsep, FALSE, FALSE, 0);
-    slot->extra_location_separator = hsep;
-
-    slot->title = g_strdup (_("Loading..."));
-#endif
 }
 
 static void
 gof_window_slot_class_init (GOFWindowSlotClass *class)
 {
-    /*class->active = real_active;
-      class->inactive = real_inactive;
-      class->update_query_editor = real_update_query_editor; */
-
     signals[ACTIVE] =
 	g_signal_new ("active",
 		      G_TYPE_FROM_CLASS (class),
@@ -338,110 +170,4 @@ gof_window_slot_get_location_uri (GOFWindowSlot *slot)
     return NULL;
 }
 
-#if 0
-char *
-nautilus_window_slot_get_title (GOFWindowSlot *slot)
-{
-    g_assert (GOF_IS_WINDOW_SLOT (slot));
-
-    if (slot->content_view != NULL) {
-        title = nautilus_view_get_title (slot->content_view);
-    }
-
-    if (title == NULL) {
-        title = nautilus_compute_title_for_location (slot->location);
-    }
-
-    return title;
-}
-#endif
-
-#if 0
-/* nautilus_window_slot_update_icon:
- * 
- * Re-calculate the slot icon
- * Called when the location or view or icon set has changed.
- * @slot: The NautilusWindowSlot in question.
- */
-void
-nautilus_window_slot_update_icon (NautilusWindowSlot *slot)
-{
-    NautilusWindow *window;
-    NautilusIconInfo *info;
-    const char *icon_name;
-    GdkPixbuf *pixbuf;
-
-    window = slot->pane->window;
-
-    g_return_if_fail (NAUTILUS_IS_WINDOW (window));
-
-    info = EEL_CALL_METHOD_WITH_RETURN_VALUE (NAUTILUS_WINDOW_CLASS, window,
-                                              get_icon, (window, slot));
-
-    icon_name = NULL;
-    if (info) {
-        icon_name = nautilus_icon_info_get_used_name (info);
-        if (icon_name != NULL) {
-            /* Gtk+ doesn't short circuit this (yet), so avoid lots of work
-             * if we're setting to the same icon. This happens a lot e.g. when
-             * the trash directory changes due to the file count changing.
-             */
-            if (g_strcmp0 (icon_name, gtk_window_get_icon_name (GTK_WINDOW (window))) != 0) {			
-                gtk_window_set_icon_name (GTK_WINDOW (window), icon_name);
-            }
-        } else {
-            pixbuf = nautilus_icon_info_get_pixbuf_nodefault (info);
-
-            if (pixbuf) {
-                gtk_window_set_icon (GTK_WINDOW (window), pixbuf);
-                g_object_unref (pixbuf);
-            } 
-        }
-
-        g_object_unref (info);
-    }
-}
-#endif
-
-#if 0
-static void
-remove_all (GtkWidget *widget,
-            gpointer data)
-{
-    GtkContainer *container;
-    container = GTK_CONTAINER (data);
-
-    gtk_container_remove (container, widget);
-}
-
-void
-nautilus_window_slot_remove_extra_location_widgets (NautilusWindowSlot *slot)
-{
-    gtk_container_foreach (GTK_CONTAINER (slot->extra_location_widgets),
-                           remove_all,
-                           slot->extra_location_widgets);
-    gtk_widget_hide (slot->extra_location_event_box);
-    gtk_widget_hide (slot->extra_location_separator);
-}
-
-void
-nautilus_window_slot_add_extra_location_widget (NautilusWindowSlot *slot,
-                                                GtkWidget *widget)
-{
-    gtk_box_pack_start (GTK_BOX (slot->extra_location_widgets),
-                        widget, TRUE, TRUE, 0);
-    gtk_widget_show (slot->extra_location_event_box);
-    gtk_widget_show (slot->extra_location_separator);
-}
-
-void
-nautilus_window_slot_add_current_location_to_history_list (NautilusWindowSlot *slot)
-{
-
-    if ((slot->pane->window == NULL || !NAUTILUS_IS_DESKTOP_WINDOW (slot->pane->window)) &&
-        nautilus_add_bookmark_to_history_list (slot->current_location_bookmark)) {
-        nautilus_send_history_list_changed ();
-    }
-}
-#endif
 
--- src/marlin-window-columns.c
+++ src/marlin-window-columns.c
@@ -18,6 +18,7 @@
  */
 
 #include "marlin-window-columns.h"
+#include <gdk/gdkkeysyms.h>
 //#include "gof-directory-async.h"
 //#include "fm-list-view.h"
 #include "fm-columns-view.h"
@@ -39,6 +40,60 @@ hadj_changed (GtkAdjustment *hadj, gpointer user_data)
     gtk_adjustment_value_changed (hadj);
 }
 
+/**
+ * Handle key release events, like the left and right keys to change the
+ * active column. It is a GObject callback.
+ **/
+static gboolean marlin_window_columns_key_pressed(GtkWidget* box, GdkEventKey* event, MarlinWindowColumns* mwcols)
+{
+    GOFWindowSlot* to_active = NULL;
+    /* The active slot position in the GList where there are all the slots */
+    int active_position = 0;
+    switch(event->keyval)
+    {
+    case GDK_KEY_Left:
+        active_position = g_list_index(mwcols->slot, mwcols->active_slot);
+        /* Get previous file list to grab_focus on it */
+        if(active_position > 0)
+        {
+            to_active = GOF_WINDOW_SLOT(g_list_nth_data(mwcols->slot, active_position-1));
+        }
+        /* If it has been found in the GList mwcols->slot (and if it is not the first) */
+        if(to_active != NULL)
+        {
+            gtk_widget_grab_focus(to_active->view_box);
+            mwcols->active_slot = to_active;
+        }
+        break;
+
+    case GDK_KEY_Right:
+        active_position = g_list_index(mwcols->slot, mwcols->active_slot);
+        /* Get previous file list to grab_focus on it */
+        if(active_position + 1 > g_list_length(mwcols->slot))
+        {
+            printf("Select a new slot\n");
+            to_active =  GOF_WINDOW_SLOT(g_list_nth_data(mwcols->slot, active_position+1));
+        }
+        if(to_active != NULL)
+        {
+            printf("grab focus\n");
+            gtk_widget_grab_focus(to_active->view_box);
+            mwcols->active_slot = to_active;
+        }
+        break;
+    }
+    printf("Active position: %d elements number: %d\n", active_position, g_list_length(mwcols->slot));
+}
+
+/**
+ * Create a new MarlinWindowColumns
+ *
+ * @param location: a GFile, it is the location where you want start your
+ * MarlinWindowColumns
+ *
+ * @param ctab: TODO: What is it?
+ *
+ **/
 MarlinWindowColumns *
 marlin_window_columns_new (GFile *location, GObject *ctab)
 {
@@ -51,39 +106,13 @@ marlin_window_columns_new (GFile *location, GObject *ctab)
     GOFWindowSlot *slot = gof_window_slot_new (location, mwcols->ctab);
     slot->mwcols = mwcols;
     mwcols->active_slot = slot;
-
-#if 0
-    gof_window_slot_make_column_view (slot);
-
-    mwcols->colpane = gtk_hbox_new (FALSE, 0);
-    slot->colpane = mwcols->colpane;
-    gtk_widget_show (mwcols->colpane);
-    mwcols->view_box = gtk_scrolled_window_new (0, 0);
-    GtkWidget *viewport = gtk_viewport_new (0, 0);
-    gtk_viewport_set_shadow_type (GTK_VIEWPORT (viewport), GTK_SHADOW_NONE);
-    gtk_container_add (GTK_CONTAINER (viewport), mwcols->colpane);
-    gtk_widget_show (viewport);
-    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (mwcols->view_box),
-                                    GTK_POLICY_AUTOMATIC,
-                                    GTK_POLICY_NEVER);
-    gtk_widget_show (mwcols->view_box);
-    gtk_container_add (GTK_CONTAINER (mwcols->view_box), viewport);
-
-    GtkAdjustment *hadj;
-    hadj = gtk_scrolled_window_get_hadjustment (GTK_SCROLLED_WINDOW (slot->mwcols->view_box));
-
-    /* autoscroll Miller Columns */
-    g_signal_connect(hadj, "changed", (GCallback) hadj_changed, mwcols);
-
-    gof_window_column_add(slot, slot->view_box);
-
-    //gtk_container_add( GTK_CONTAINER(window), mwcols->view_box);
-    //marlin_view_window_set_content (window, mwcols->view_box);
-    marlin_view_view_container_set_content (ctab, mwcols->view_box);
-#endif
+    mwcols->slot = g_list_append(mwcols->slot, slot);
     return mwcols;
 }
 
+/**
+ * TODO: doc
+ **/
 void
 marlin_window_columns_make_view (MarlinWindowColumns *mwcols)
 {
@@ -116,127 +145,42 @@ marlin_window_columns_make_view (MarlinWindowColumns *mwcols)
     //gtk_container_add( GTK_CONTAINER(window), mwcols->view_box);
     //marlin_view_window_set_content (window, mwcols->view_box);
     marlin_view_view_container_set_content (mwcols->ctab, mwcols->view_box);
+    gtk_widget_add_events (GTK_WIDGET(mwcols->colpane), GDK_KEY_RELEASE_MASK);
+    g_signal_connect(mwcols->colpane, "key_release_event", (GCallback)marlin_window_columns_key_pressed, mwcols);
 }
 
+/**
+ * Add a new column
+ **/
 void
 marlin_window_columns_add (MarlinWindowColumns *mwcols, GFile *location)
 {
+    printf("YEAH\n\n\n\n");
     GOFWindowSlot *slot = gof_window_slot_new (location, mwcols->ctab);
     gof_window_slot_make_column_view (slot);
     slot->mwcols = mwcols;
     slot->colpane = mwcols->active_slot->colpane;
-    //mwcols->active_slot = slot;
-    //add_column(mwcols, slot->view_box);
     gof_window_column_add(slot, slot->view_box);
+    /* Add it in our GList */
+    mwcols->slot = g_list_append(mwcols->slot, slot);
 }
 
 static void
 marlin_window_columns_init (MarlinWindowColumns *mwcol)
 {
-#if 0
-    GtkWidget *content_box, *eventbox, *extras_vbox, *frame, *hsep;
-
-    content_box = gtk_vbox_new (FALSE, 0);
-    slot->content_box = content_box;
-    gtk_widget_show (content_box);
-
-    frame = gtk_frame_new (NULL);
-    gtk_frame_set_shadow_type (GTK_FRAME (frame), GTK_SHADOW_ETCHED_IN);
-    gtk_box_pack_start (GTK_BOX (content_box), frame, TRUE, TRUE, 0);
-    gtk_widget_show (frame);
-
-    slot->view_box = gtk_vbox_new (FALSE, 0);
-    gtk_container_add (GTK_CONTAINER (frame), slot->view_box);
-    gtk_widget_show (slot->view_box);
-#endif
-    //slot->view_box = GTK_WIDGET (g_object_new (FM_TYPE_LIST_VIEW, NULL));
-    /*slot->view_box = GTK_WIDGET (g_object_new (FM_TYPE_LIST_VIEW,
-      "window-slot", slot, NULL));*/
-    //gtk_box_pack_start (GTK_BOX (slot->content_box), GTK_WIDGET (slot->list_view->tree), TRUE, TRUE, 0);
-
-
-    /*GtkWidget *m_scwin;
-      m_scwin = gtk_scrolled_window_new(NULL,NULL);
-      slot->content_box = m_scwin;
-      gtk_scrolled_window_set_policy( GTK_SCROLLED_WINDOW(m_scwin), GTK_POLICY_AUTOMATIC , GTK_POLICY_AUTOMATIC);
-      gtk_scrolled_window_set_shadow_type( GTK_SCROLLED_WINDOW(m_scwin), GTK_SHADOW_NONE);*/
-
-    //gtk_container_add( GTK_CONTAINER(m_scwin), GTK_WIDGET (slot->list_view->tree));
-
-
-#if 0
-    eventbox = gtk_event_box_new ();
-    slot->extra_location_event_box = eventbox;
-    gtk_widget_set_name (eventbox, "nautilus-extra-view-widget");
-    gtk_box_pack_start (GTK_BOX (slot->view_box), eventbox, FALSE, FALSE, 0);
-
-    extras_vbox = gtk_vbox_new (FALSE, 6);
-    gtk_container_set_border_width (GTK_CONTAINER (extras_vbox), 6);
-    slot->extra_location_widgets = extras_vbox;
-    gtk_container_add (GTK_CONTAINER (eventbox), extras_vbox);
-    gtk_widget_show (extras_vbox);
-
-    hsep = gtk_hseparator_new ();
-    gtk_box_pack_start (GTK_BOX (slot->view_box), hsep, FALSE, FALSE, 0);
-    slot->extra_location_separator = hsep;
-
-    slot->title = g_strdup (_("Loading..."));
-#endif
 }
 
 static void
 marlin_window_columns_class_init (MarlinWindowColumnsClass *class)
 {
-    /*class->active = real_active;
-      class->inactive = real_inactive;
-      class->update_query_editor = real_update_query_editor; */
-
     G_OBJECT_CLASS (class)->finalize = marlin_window_columns_finalize;
 }
-
-/*GOFWindowSlot *
-  marlin_window_columns_get_active_slot (MarlinWindowColumns *mwcols)
-  {
-  return (mwcols->active_slot);
-  }*/
 
 GFile *
 marlin_window_columns_get_location (MarlinWindowColumns *mwcols)
 {
     return mwcols->location;
 }
-
-#if 0
-void
-nautilus_window_slot_remove_extra_location_widgets (NautilusWindowSlot *slot)
-{
-    gtk_container_foreach (GTK_CONTAINER (slot->extra_location_widgets),
-                           remove_all,
-                           slot->extra_location_widgets);
-    gtk_widget_hide (slot->extra_location_event_box);
-    gtk_widget_hide (slot->extra_location_separator);
-}
-
-void
-nautilus_window_slot_add_extra_location_widget (NautilusWindowSlot *slot,
-                                                GtkWidget *widget)
-{
-    gtk_box_pack_start (GTK_BOX (slot->extra_location_widgets),
-                        widget, TRUE, TRUE, 0);
-    gtk_widget_show (slot->extra_location_event_box);
-    gtk_widget_show (slot->extra_location_separator);
-}
-
-void
-nautilus_window_slot_add_current_location_to_history_list (NautilusWindowSlot *slot)
-{
-
-    if ((slot->pane->window == NULL || !NAUTILUS_IS_DESKTOP_WINDOW (slot->pane->window)) &&
-        nautilus_add_bookmark_to_history_list (slot->current_location_bookmark)) {
-        nautilus_send_history_list_changed ();
-    }
-}
-#endif
 
 static void
 marlin_window_columns_finalize (GObject *object)
@@ -247,39 +191,5 @@ marlin_window_columns_finalize (GObject *object)
     g_object_unref(mwcols->location);
 
     G_OBJECT_CLASS (parent_class)->finalize (object);
-
-#if 0
-    GtkWidget *widget;
-
-    if (slot->content_view) {
-        widget = nautilus_view_get_widget (slot->content_view);
-        gtk_widget_destroy (widget);
-        g_object_unref (slot->content_view);
-        slot->content_view = NULL;
-    }
-
-    if (slot->new_content_view) {
-        widget = nautilus_view_get_widget (slot->new_content_view);
-        gtk_widget_destroy (widget);
-        g_object_unref (slot->new_content_view);
-        slot->new_content_view = NULL;
-    }
-
-    nautilus_window_slot_set_viewed_file (slot, NULL);
-    /* TODO? why do we unref here? the file is NULL.
-     * It was already here before the slot move, though */
-    nautilus_file_unref (slot->viewed_file);
-
-
-    eel_g_list_free_deep (slot->pending_selection);
-    slot->pending_selection = NULL;
-
-    g_free (slot->title);
-    slot->title = NULL;
-
-    g_free (slot->status_text);
-    slot->status_text = NULL;
-#endif
-    //G_OBJECT_CLASS (parent_class)->dispose (object);
 }
 
