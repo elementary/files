1cac1b6058ae3dbcdeee15496e64e9c8c269036f
xapantu <xapantu@gmail.com>
2011-08-27 22:00:23 +0200

    Fix some things in the build system

--- marlinplugins/marlinplugins.pc.cmake
+++ marlinplugins/marlinplugins.pc.cmake
@@ -6,7 +6,7 @@ includedir=@DOLLAR@{prefix}/include/
 Name: @PKGNAME@
 Description: Marlin plugin library
 Version: 0.1
-Libs: -L@DOLLAR@{libdir} -lmarlinplugin
+Libs: -L@DOLLAR@{libdir} -lmarlinplugins
 Cflags: -I@DOLLAR@{includedir}/${PKGNAME}
 Requires: gtk+-3.0 glib-2.0 gio-2.0 gee-1.0
 
