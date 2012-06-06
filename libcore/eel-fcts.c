708c95baf962a29fe1410daf373dde91fef084e3
am.monkeyd@gmail.com <>
2012-06-07 01:03:49 +0200

    use gtk_application_inhibit/uninhibit use g_date_time_format don't use eel_round marlin-dnd clean up remove dead code

--- libcore/eel-fcts.c
+++ libcore/eel-fcts.c
@@ -27,196 +27,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
-#include <sys/types.h>
+//#include <sys/types.h>
 #include <pwd.h>
 #include <grp.h>
 #include "eel-glib-extensions.h"
 #include "eel-i18n.h"
-
-#define C_STANDARD_STRFTIME_CHARACTERS "aAbBcdHIjmMpSUwWxXyYZ"
-#define C_STANDARD_NUMERIC_STRFTIME_CHARACTERS "dHIjmMSUwWyY"
-#define SUS_EXTENDED_STRFTIME_MODIFIERS "EO"
-
-/**
- * eel_strdup_strftime:
- *
- * Cover for standard date-and-time-formatting routine strftime that returns
- * a newly-allocated string of the correct size. The caller is responsible
- * for g_free-ing the returned string.
- *
- * Besides the buffer management, there are two differences between this
- * and the library strftime:
- *
- *   1) The modifiers "-" and "_" between a "%" and a numeric directive
- *      are defined as for the GNU version of strftime. "-" means "do not
- *      pad the field" and "_" means "pad with spaces instead of zeroes".
- *   2) Non-ANSI extensions to strftime are flagged at runtime with a
- *      warning, so it's easy to notice use of the extensions without
- *      testing with multiple versions of the library.
- *
- * @format: format string to pass to strftime. See strftime documentation
- * for details.
- * @time_pieces: date/time, in struct format.
- * 
- * Return value: Newly allocated string containing the formatted time.
-**/
-char *
-eel_strdup_strftime (const char *format, struct tm *time_pieces)
-{
-    GString *string;
-    const char *remainder, *percent;
-    char code[4], buffer[512];
-    char *piece, *result, *converted;
-    size_t string_length;
-    gboolean strip_leading_zeros, turn_leading_zeros_to_spaces;
-    char modifier;
-    int i;
-
-    /* Format could be translated, and contain UTF-8 chars,
-     * so convert to locale encoding which strftime uses */
-    converted = g_locale_from_utf8 (format, -1, NULL, NULL, NULL);
-    g_return_val_if_fail (converted != NULL, NULL);
-
-    string = g_string_new ("");
-    remainder = converted;
-
-    /* Walk from % character to % character. */
-    for (;;) {
-        percent = strchr (remainder, '%');
-        if (percent == NULL) {
-            g_string_append (string, remainder);
-            break;
-        }
-        g_string_append_len (string, remainder,
-                             percent - remainder);
-
-        /* Handle the "%" character. */
-        remainder = percent + 1;
-        switch (*remainder) {
-        case '-':
-            strip_leading_zeros = TRUE;
-            turn_leading_zeros_to_spaces = FALSE;
-            remainder++;
-            break;
-        case '_':
-            strip_leading_zeros = FALSE;
-            turn_leading_zeros_to_spaces = TRUE;
-            remainder++;
-            break;
-        case '%':
-            g_string_append_c (string, '%');
-            remainder++;
-            continue;
-        case '\0':
-            g_warning ("Trailing %% passed to eel_strdup_strftime");
-            g_string_append_c (string, '%');
-            continue;
-        default:
-            strip_leading_zeros = FALSE;
-            turn_leading_zeros_to_spaces = FALSE;
-            break;
-        }
-
-        modifier = 0;
-        if (strchr (SUS_EXTENDED_STRFTIME_MODIFIERS, *remainder) != NULL) {
-            modifier = *remainder;
-            remainder++;
-
-            if (*remainder == 0) {
-                g_warning ("Unfinished %%%c modifier passed to eel_strdup_strftime", modifier);
-                break;
-            }
-        } 
-
-        if (strchr (C_STANDARD_STRFTIME_CHARACTERS, *remainder) == NULL) {
-            g_warning ("eel_strdup_strftime does not support "
-                       "non-standard escape code %%%c",
-                       *remainder);
-        }
-
-        /* Convert code to strftime format. We have a fixed
-         * limit here that each code can expand to a maximum
-         * of 512 bytes, which is probably OK. There's no
-         * limit on the total size of the result string.
-         */
-        i = 0;
-        code[i++] = '%';
-        if (modifier != 0) {
-#ifdef HAVE_STRFTIME_EXTENSION
-            code[i++] = modifier;
-#endif
-        }
-        code[i++] = *remainder;
-        code[i++] = '\0';
-        string_length = strftime (buffer, sizeof (buffer),
-                                  code, time_pieces);
-        if (string_length == 0) {
-            /* We could put a warning here, but there's no
-             * way to tell a successful conversion to
-             * empty string from a failure.
-             */
-            buffer[0] = '\0';
-        }
-
-        /* Strip leading zeros if requested. */
-        piece = buffer;
-        if (strip_leading_zeros || turn_leading_zeros_to_spaces) {
-            if (strchr (C_STANDARD_NUMERIC_STRFTIME_CHARACTERS, *remainder) == NULL) {
-                g_warning ("eel_strdup_strftime does not support "
-                           "modifier for non-numeric escape code %%%c%c",
-                           remainder[-1],
-                           *remainder);
-            }
-            if (*piece == '0') {
-                do {
-                    piece++;
-                } while (*piece == '0');
-                if (!g_ascii_isdigit (*piece)) {
-                    piece--;
-                }
-            }
-            if (turn_leading_zeros_to_spaces) {
-                memset (buffer, ' ', piece - buffer);
-                piece = buffer;
-            }
-        }
-        remainder++;
-
-        /* Add this piece. */
-        g_string_append (string, piece);
-    }
-
-    /* Convert the string back into utf-8. */
-    result = g_locale_to_utf8 (string->str, -1, NULL, NULL, NULL);
-
-    g_string_free (string, TRUE);
-    g_free (converted);
-
-    return result;
-}
-
-/**
- * eel_g_date_new_tm:
- * 
- * Get a new GDate * for the date represented by a tm struct. 
- * The caller is responsible for g_free-ing the result.
- * @time_pieces: Pointer to a tm struct representing the date to be converted.
- * 
- * Returns: Newly allocated string formated date.
- * 
-**/
-GDate *
-eel_g_date_new_tm (struct tm *time_pieces)
-{
-    /* tm uses 0-based months; GDate uses 1-based months.
-     * tm_year needs 1900 added to get the full year.
-     */
-    return g_date_new_dmy (time_pieces->tm_mday,
-                           time_pieces->tm_mon + 1,
-                           time_pieces->tm_year + 1900);
-}
-
 
 static const char *TODAY_TIME_FORMATS [] = {
     /* Today, use special word.
@@ -237,6 +52,9 @@ static const char *TODAY_TIME_FORMATS [] = {
     N_("Today at 00:00:00 PM"),
     N_("Today at %-I:%M:%S %p"),
 
+    N_("Today at 00:00:00 PM"),
+    N_("Today at %-I:%M:%S %p"),
+
     N_("Today at 00:00 PM"),
     N_("Today at %-I:%M %p"),
 
@@ -253,6 +71,9 @@ static const char *YESTERDAY_TIME_FORMATS [] = {
     /* Yesterday, use special word.
      * Note to localizers: Same issues as "Today" string.
      */
+    N_("Yesterday at 00:00:00 PM"),
+    N_("Yesterday at %-I:%M:%S %p"),
+
     N_("Yesterday at 00:00:00 PM"),
     N_("Yesterday at %-I:%M:%S %p"),
 
@@ -280,8 +101,10 @@ static const char *CURRENT_WEEK_TIME_FORMATS [] = {
     N_("Mon, Oct 00 0000 at 00:00:00 PM"),
     N_("%a, %b %-d %Y at %-I:%M:%S %p"),
 
-    N_("Mon, Oct 00 0000 at 00:00 PM"),
-    N_("%a, %b %-d %Y at %-I:%M %p"),
+    /*N_("Mon, Oct 00 0000 at 00:00 PM"),
+    N_("%a, %b %-d %Y at %-I:%M %p"),*/
+    N_("Mon 00 Oct 0000 at 00:00 PM"),
+    N_("%a %-d %b %Y at %-I:%M %p"),
 
     N_("Oct 00 0000 at 00:00 PM"),
     N_("%b %-d %Y at %-I:%M %p"),
@@ -312,35 +135,28 @@ static const char *CURRENT_WEEK_TIME_FORMATS [] = {
 char *
 eel_get_date_as_string (guint64 d, gchar *date_format)
 {
-    struct tm *file_time;
     const char **formats;
-    const char *width_template;
+    //const char *width_template;
     const char *format;
-    char *date_string;
-    GDate *today;
-    GDate *file_date;
-    guint32 file_date_age;
-    int i;
+    gchar *result = NULL;
+    //int i;
+    GDateTime *date_time, *today;
+    GTimeSpan file_date_age;
 
     g_return_val_if_fail (date_format != NULL, NULL);
-    file_time = localtime (&d);
+    date_time = g_date_time_new_from_unix_local ((gint64) d);
 
-    if (!strcmp (date_format, "locale"))
-        return eel_strdup_strftime ("%c", file_time);
-    else if (!strcmp (date_format, "iso"))
-        return eel_strdup_strftime ("%Y-%m-%d %H:%M:%S", file_time);
+    if (!strcmp (date_format, "locale")) {
+        result = g_date_time_format (date_time, "%c");
+		goto out;
+    } else if (!strcmp (date_format, "iso")) {
+        result = g_date_time_format (date_time, "%Y-%m-%d %H:%M:%S");
+		goto out;
+    }
 
-    file_date = eel_g_date_new_tm (file_time);
-
-    today = g_date_new ();
-    g_date_set_time_t (today, time (NULL));
-
-    /* Overflow results in a large number; fine for our purposes. */
-    file_date_age = (g_date_get_julian (today) -
-                     g_date_get_julian (file_date));
-
-    g_date_free (file_date);
-    g_date_free (today);
+    today = g_date_time_new_now_local ();
+	file_date_age = g_date_time_difference (today, date_time);
+	g_date_time_unref (today);
 
     /* Format varies depending on how old the date is. This minimizes
      * the length (and thus clutter & complication) of typical dates
@@ -350,16 +166,15 @@ eel_get_date_as_string (guint64 d, gchar *date_format)
      * internationalization's sake.
      */
 
-    if (file_date_age == 0)	{
+    if (file_date_age < G_TIME_SPAN_DAY) {
         formats = TODAY_TIME_FORMATS;
-    } else if (file_date_age == 1) {
+    } else if (file_date_age < 2 * G_TIME_SPAN_DAY) {
         formats = YESTERDAY_TIME_FORMATS;
-    } else if (file_date_age < 7) {
-        formats = CURRENT_WEEK_TIME_FORMATS;
     } else {
         formats = CURRENT_WEEK_TIME_FORMATS;
     }
 
+#if 0
     /* Find the date format that just fits the required width. Instead of measuring
      * the resulting string width directly, measure the width of a template that represents
      * the widest possible version of a date in a given format. This is done by using M, m
@@ -377,9 +192,7 @@ eel_get_date_as_string (guint64 d, gchar *date_format)
              * shortest format
              */
 
-            date_string = eel_strdup_strftime (format, file_time);
-
-            return date_string;
+            result = g_date_time_format (date_time, format);
         }
 
         format = _(formats [i + 1]);
@@ -388,7 +201,16 @@ eel_get_date_as_string (guint64 d, gchar *date_format)
         break;
     }
 
-    return eel_strdup_strftime (format, file_time);
+    if (result == NULL) {
+		result = g_date_time_format (date_time, format);
+	}
+#endif
+    format = _(formats [5]);
+	result = g_date_time_format (date_time, format);
+
+ out:
+    g_date_time_unref (date_time);
+    return result;
 }
 
 /**
