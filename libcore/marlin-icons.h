ed11a545302c5d87601d1495a11c4cb5519cec16
Cody Garver <cody@elementaryos.org>
2013-05-12 00:06:42 -0500

    Another drive-harddisk-system to drive-harddisk.

--- libcore/marlin-icons.h
+++ libcore/marlin-icons.h
@@ -3,7 +3,7 @@
 #define MARLIN_ICON_TRASH_FULL      "user-trash-full"
 #define MARLIN_ICON_NETWORK         "network-workgroup"
 #define MARLIN_ICON_NETWORK_SERVER  "network-server"
-#define MARLIN_ICON_FILESYSTEM      "drive-harddisk-system"
+#define MARLIN_ICON_FILESYSTEM      "drive-harddisk"
 
 #define MARLIN_TRASH_URI            "trash:///"
 #define MARLIN_NETWORK_URI          "network:///"
