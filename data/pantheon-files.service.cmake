1be576e678f78cd23ee653a70e86d86a8bc37d2d
xapantu <xapantu@gmail.com>
2014-08-05 21:38:09 +0200

    Renaming

--- data/pantheon-files.service.cmake
+++ data/pantheon-files.service.cmake
@@ -1,3 +1,3 @@
 [D-BUS Service]
-Name=org.elementary.marlin.db
-Exec=@CMAKE_INSTALL_PREFIX@/bin/marlin-daemon
+Name=org.elementary.pantheon-files.db
+Exec=@CMAKE_INSTALL_PREFIX@/bin/pantheon-files-daemon
