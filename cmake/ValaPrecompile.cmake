c53ccfec7a73a12536dbd3ab7e37d8d18ba1b166
Jeremy Wootten <jeremy@elementaryos.org>
2014-12-21 13:22:50 +0000

    Remove --save-temps option from ValaPrecompile.cmake

--- cmake/ValaPrecompile.cmake
+++ cmake/ValaPrecompile.cmake
@@ -229,7 +229,6 @@ macro(vala_precompile output target_name)
         ${vala_pkg_opts} 
         ${ARGS_OPTIONS} 
         "-g"
-        "--save-temps"
         ${in_files} 
         ${custom_vapi_arguments}
     COMMAND
