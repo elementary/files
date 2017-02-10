0197ea4d6aa2792035f9d379d2704fc5f6604920
Jeremy Wootten <jeremy@elementaryos.org>
2017-02-10 19:35:18 +0000

    Fix licensecheck warnings

--- libcore/tests/MarlinIconInfoTests/tests.c
+++ libcore/tests/MarlinIconInfoTests/tests.c
@@ -3,7 +3,7 @@
  *
  * Marlin is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
+ * published by the Free Software Foundation, Inc.,; either version 2 of the
  * License, or (at your option) any later version.
  *
  * Marlin is distributed in the hope that it will be useful,
@@ -12,8 +12,8 @@
  * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335 USA.
  *
  */
 
