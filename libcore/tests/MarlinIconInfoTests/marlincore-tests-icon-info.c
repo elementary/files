66f0eb16e3a607afdd7e944a4a83e800993ddd44
Jeremy Wootten <jeremy@elementaryos.org>
2017-06-22 08:56:22 +0100

    Remove unused debugging code and correct some signatures and formatting (#34)
    * Remove unused debugging code; Correct some signatures; Code format.
    * Function for printing out the contents of the icon caches removed, as not useful for CI.
    * Remove message in test not useful for CI. (to be replaced with test)
    * Correct signatures of some functions returning Pixbufs.
    * Insert some missing spaces.
    * Remove remaining commented out code.

--- libcore/tests/MarlinIconInfoTests/marlincore-tests-icon-info.c
+++ libcore/tests/MarlinIconInfoTests/marlincore-tests-icon-info.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335 USA.
- * 
+ *
  */
 
 #include <stdlib.h>
@@ -33,33 +33,18 @@ static gboolean fatal_handler(const gchar* log_domain,
     return FALSE;
 }
 
-static gboolean
-show_infos (gpointer data)
-{
-    marlin_icon_info_infos_caches ();
-    g_main_loop_quit (loop);
-
-    return FALSE;
-}
-
 void marlincore_tests_icon_info (void)
 {
     GOFFile* file;
     g_test_log_set_fatal_handler (fatal_handler, NULL);
-    /* The URI is valid, the target exists */
+    /* The URI is valid, the target must exist on Travis CI */
     file = gof_file_get_by_uri ("file:///usr/share/icons/hicolor/16x16/apps/system-file-manager.svg");
     g_assert(file != NULL);
     gof_file_query_update (file);
     g_assert(file->pix == NULL);
-    file->flags = 2;
+    file->flags = 2; /* ThumbState.READY */
     gof_file_update_icon (file, 128);
     g_assert(file->pix != NULL);
     gof_file_update_icon (file, 32);
-    /*gof_file_update_icon (file, 16);*/
-    g_message ("pix ref count %u", G_OBJECT (file->pix)->ref_count);
     g_object_unref (file->pix);
-    g_timeout_add_seconds_full (0, 2, show_infos, NULL, NULL);
-
-    loop = g_main_loop_new(NULL, FALSE);
-    g_main_loop_run(loop);
 }
