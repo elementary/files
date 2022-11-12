elementary/files

commit 53983bde62c45d3c10557a50ee7ae982e0c53abd

More work towards the Vala port (#2092)
* FileOperations: Refine the structure types in prototypes

* FileOperations: Remove OP_KIND_TRASH

It is not used anymore

* FileOperation: Start splitting the operations

We only want to keep the common code paths.

* FileOperations: Port the main structures to Vala

Allows to easily handle the creation/destruction functions.

Co-authored-by: Jeremy Wootten <jeremy@elementaryos.org>

libcore/FileOperations/CreateJob.vala
@@ -1,0 +1,51 @@
+/* Copyright 2022 elementary LLC (https://elementary.io)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, Inc.,; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+public class Files.FileOperations.CreateJob : CommonJob {
+    protected GLib.File dest_dir;
+    protected string? filename;
+    protected bool make_dir;
+    protected GLib.File? src;
+    [CCode (array_length_cname = "length")]
+    protected uint8[]? src_data;
+    protected GLib.File created_file;
+
+    ~CreateJob () {
+        Files.FileChanges.consume_changes (true);
+    }
+
+    public CreateJob.folder (Gtk.Window? parent_window, GLib.File dest_dir) {
+        base (parent_window);
+        this.dest_dir = dest_dir;
+        this.make_dir = true;
+    }
+
+    public CreateJob.file_from_template (Gtk.Window? parent_window, GLib.File dest_dir, string target_filename, GLib.File? src) {
+        base (parent_window);
+        this.dest_dir = dest_dir;
+        this.filename = target_filename;
+        this.src = src;
+    }
+
+    public CreateJob.file (Gtk.Window? parent_window, GLib.File dest_dir, string target_filename, [CCode (array_length_cname = "length")] uint8[] src_data) {
+        base (parent_window);
+        this.dest_dir = dest_dir;
+        this.filename = target_filename;
+        this.src_data = src_data;
+    }
+}

libcore/FileOperations/CommonJob.vala
@@ -22,12 +22,17 @@ public class Files.FileOperations.CommonJob {
     protected unowned GLib.Cancellable? cancellable;
     protected PF.Progress.Info progress;
     protected Files.UndoActionData? undo_redo_data;
+    protected GLib.Timer time;
+    protected bool skip_all_error;
+    protected GLib.HashTable<GLib.File, bool>? skip_readdir_error;
+    protected GLib.HashTable<GLib.File, unowned GLib.File>? skip_files;
     protected CommonJob (Gtk.Window? parent_window = null) {
         this.parent_window = parent_window;
         inhibit_cookie = 0;
         progress = new PF.Progress.Info ();
         cancellable = progress.cancellable;
         undo_redo_data = null;
+        time = new GLib.Timer ();
     }
 
     ~CommonJob () {

libcore/FileOperations/CopyMoveJob.vala
@@ -1,0 +1,45 @@
+/* Copyright 2022 elementary LLC (https://elementary.io)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, Inc.,; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+public class Files.FileOperations.CopyMoveJob : CommonJob {
+    protected bool is_move = false;
+    protected GLib.List<GLib.File> files;
+    protected GLib.File? destination;
+    protected GLib.HashTable<GLib.File,bool> debuting_files = new GLib.HashTable<GLib.File,bool> (GLib.File.hash, GLib.File.equal);
+    protected bool replace_all = false;
+    protected bool merge_all = false;
+    protected bool keep_all_newest = false;
+    protected bool skip_all_conflict = false;
+
+    ~CopyMoveJob () {
+        Files.FileChanges.consume_changes (true);
+    }
+
+    public CopyMoveJob (Gtk.Window? parent_window, GLib.List<GLib.File> files, GLib.File? destination) {
+        base (parent_window);
+        this.files = files.copy_deep ((GLib.CopyFunc<GLib.File>) GLib.Object.ref);
+        this.destination = destination;
+    }
+
+    public CopyMoveJob.move (Gtk.Window? parent_window, GLib.List<GLib.File> files, GLib.File? destination) {
+        base (parent_window);
+        this.files = files.copy_deep ((GLib.CopyFunc<GLib.File>) GLib.Object.ref);
+        this.destination = destination;
+        is_move = true;
+    }
+}

libcore/FileOperations/DeleteJob.vala
@@ -1,0 +1,35 @@
+/* Copyright 2022 elementary LLC (https://elementary.io)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, Inc.,; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+public class Files.FileOperations.DeleteJob : CommonJob {
+    protected GLib.List<GLib.File> files;
+    protected bool try_trash;
+    protected bool user_cancel;
+    protected bool delete_all;
+
+    ~DeleteJob () {
+        Files.FileChanges.consume_changes (true);
+    }
+
+    public DeleteJob (Gtk.Window? parent_window, GLib.List<GLib.File> files, bool try_trash) {
+        base (parent_window);
+        this.files = files.copy_deep ((GLib.CopyFunc<GLib.File>) GLib.Object.ref);
+        this.try_trash = try_trash;
+        this.user_cancel = false;
+    }
+}

libcore/meson.build
@@ -42,6 +42,9 @@ pantheon_files_core_vala_files = files(
     'UserUtils.vala',
     'UndoManager.vala',
     'FileOperations/CommonJob.vala',
+    'FileOperations/CopyMoveJob.vala',
+    'FileOperations/CreateJob.vala',
+    'FileOperations/DeleteJob.vala',
     'FileOperations/EmptyTrashJob.vala',
     'FileOperations/MountUtils.vala',
     'FileOperations/DialogUtils.vala',

commit 08673f03c4f9388eb7c1409d5de6d82a8303aee5

FileOperations: Fix crash creating new empty file
Creating a new empty file (Right click -> New -> Empty File) crashes
Files.

There were 2 issues causing the crash:

 - `CreateJob.file` didn't allow to set an empty `target_filename`.
 - `create_job ()` accessed uninitialized memory.

This commit fixes both issues.

Fix: https://github.com/elementary/files/issues/2098
Fixes: 53983bde62c4 ("More work towards the Vala port (#2092)")

libcore/FileOperations/CreateJob.vala
@@ -42,7 +42,7 @@ public class Files.FileOperations.CreateJob : CommonJob {
         this.src = src;
     }
 
-    public CreateJob.file (Gtk.Window? parent_window, GLib.File dest_dir, string target_filename, [CCode (array_length_cname = "length")] uint8[] src_data) {
+    public CreateJob.file (Gtk.Window? parent_window, GLib.File dest_dir, string? target_filename, [CCode (array_length_cname = "length")] uint8[] src_data) {
         base (parent_window);
         this.dest_dir = dest_dir;
         this.filename = target_filename;

libcore/marlin-file-operations.c
@@ -4394,7 +4394,7 @@ retry:
             }
             // End UNDO-REDO
         } else {
-            data[0] = '\0';
+            data = "";
             length = 0;
             if (job->src_data) {
                 data = job->src_data;