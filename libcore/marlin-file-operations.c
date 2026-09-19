8422adcb26967ac14d86fdeab1aff502de89e35b
Vishal Rao <vishalrao@gmail.com>
2026-09-19 14:18:51 +0530

    Fix refcount crash

--- libcore/marlin-file-operations.c
+++ libcore/marlin-file-operations.c
@@ -1645,7 +1645,10 @@ copy_move_file (FilesFileOperationsCopyMoveJob *copy_job,
         goto out;
     }
 
-    copy_job->destination = dest_dir;
+    if (copy_job->destination == NULL) {
+        copy_job->destination = dest_dir;
+        g_object_ref (dest_dir);
+    }
 
 retry:
 
