48a233bff595ed38a171498f290857ce0a13fde9
Vishal Rao <vishalrao@gmail.com>
2026-09-19 12:18:23 +0530

    Remove extraneous final sync

--- libcore/marlin-file-operations.c
+++ libcore/marlin-file-operations.c
@@ -2248,8 +2248,6 @@ retry:
             g_hash_table_replace (debuting_files, g_object_ref (dest), GINT_TO_POINTER (TRUE));
         }
 
-        files_file_utils_sync (dest);
-
         files_file_changes_queue_file_moved (src, dest);
 
         /*if (position) {
